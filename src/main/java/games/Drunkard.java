799487af4a70f174e8725d8ae4850bd96e4f27b3
Artem Malafeev <mrtema1999@gmail.com>
2019-02-23 15:40:44 +0300

    task 5

--- src/main/java/games/Drunkard.java
+++ src/main/java/games/Drunkard.java
@@ -2,29 +2,79 @@ package games;
 
 import org.apache.commons.math3.util.MathArrays;
 
+import static org.apache.commons.math3.util.MathArrays.ebeMultiply;
 import static org.apache.commons.math3.util.MathArrays.shuffle;
 
 public class Drunkard {
 
+
     private static final int PARS_TOTAL_COUNT = Par.values().length;
+
     private static final int CARDS_TOTAL_COUNT = PARS_TOTAL_COUNT * Suit.values().length;
-    private static int[] firstPlayersCards = new int[CARDS_TOTAL_COUNT];
-    private static int[] secondPlayersCards = new int[CARDS_TOTAL_COUNT];
-    private static int[] deckOfCards = new int[CARDS_TOTAL_COUNT];
 
-    public static void main(String... __) {
+    private static int[][] playersCards = new int[2][CARDS_TOTAL_COUNT + 1];
 
-        for (int i = 0; i < deckOfCards.length; i++) {
-            deckOfCards[i] = i;
+    private static int[] playersCardTails = new int[2];
+    private static int[] playersCardHeads = new int[2];
+
+    private static int Player1 = 0;
+    private static int Player2 = 1;
+
+    private static int[] deckCards = new int[CARDS_TOTAL_COUNT];
+
+    public static void main(String[] args) {
+
+        shufflingCards();
+        distributionCards();
+
+        playersCardTails[Player1] = 0;
+        playersCardTails[Player2] = 0;
+        playersCardHeads[Player1] = deckCards.length / 2;
+        playersCardHeads[Player2] = deckCards.length / 2;
+
+        int counter = 1;
+
+        while (true) {
+            System.out.println("Итерация " + counter + "! Игрок №1 карта: " + toString(playersCards[Player1][playersCardTails[Player1]]) +
+                "; Игрок №2 карта: " + toString(playersCards[Player2][playersCardTails[Player2]]));
+
+            int result = moveWinner(playersCardTails[Player1], playersCardTails[Player2]);
+
+            switch (result) {
+
+                case 0: {
+                    System.out.println("Выиграл игрок №1\n");
+                    winFirstPlayer();
+                    showCountOfCards(result);
+                    break;
+             }
+                case 1: {
+                    System.out.println("Выиграл игрок №2\n");
+                    winSecondPlayer();
+                    showCountOfCards(result);
+                    break;
+                }
+
+                case 2: {
+                    System.out.println("Спор - каждый остается при своих\n");
+                    friendlyWon();
+                    showCountOfCards(result);
+                    break;
+                }
+            }
+
+
+            if (playerCardsIsEmpty(Player1) && result == 0) {
+                System.out.println("Выиграл первый игрок. Количество произведенных итераций: " + counter);
+                break;
+            } else if (playerCardsIsEmpty(Player2) && result == 1) {
+                System.out.println("Выиграл второй игрок. Количество произведенных итераций:" + counter);
+                break;
+            } else {
+                counter++;
+            }
+
         }
-
-        MathArrays.shuffle(deckOfCards);
-
-        for (int i = 0; i < deckOfCards.length / 2; i++) {
-            firstPlayersCards[i] = deckOfCards[i];
-            secondPlayersCards[i] = deckOfCards[18 + i];
-        }
-
     }
 
     enum Suit {
@@ -46,12 +96,133 @@ public class Drunkard {
         AVE
     }
 
+
+    // Метод проверки победиля хода
+    private static int moveWinner(int cardFirstPlayer, int cardSecondPlayer) {
+
+        int valueFirstCard = getValueCard(playersCards[Player1][cardFirstPlayer]);
+        int valueSecondCard = getValueCard(playersCards[Player2][cardSecondPlayer]);
+
+        if(valueFirstCard == 0 && valueSecondCard == 8) {
+            return 0;
+        } else if (valueSecondCard == 0 && valueFirstCard == 8) {
+            return 1;
+        } else if (valueFirstCard > valueSecondCard) {
+            return 0;
+        } else if (valueFirstCard == valueSecondCard) {
+            return 2;
+        } else {
+            return 1;
+        }
+    }
+
+    private static void winFirstPlayer() {
+
+        playersCards[Player1][playersCardHeads[Player1]] = playersCardTails[Player1];
+        playersCards[Player1][playersCardHeads[Player1] + 1] = playersCardTails[Player2];
+
+        playersCardTails[Player1] = incrementIndex(playersCardTails[Player1]);
+        playersCardHeads[Player1] = incrementIndex(playersCardHeads[Player1]);
+        playersCardHeads[Player1] = incrementIndex(playersCardHeads[Player1]);
+        playersCardTails[Player2] = incrementIndex(playersCardTails[Player2]);
+    }
+
+    private static void winSecondPlayer() {
+
+        playersCards[Player2][playersCardHeads[Player2]] = playersCardTails[Player2];
+        playersCards[Player2][playersCardHeads[Player2] + 1] = playersCardTails[Player1];
+
+        playersCardTails[Player2] = incrementIndex(playersCardTails[Player2]);
+        playersCardHeads[Player2] = incrementIndex(playersCardHeads[Player2]);
+        playersCardHeads[Player2] = incrementIndex(playersCardHeads[Player2]);
+        playersCardTails[Player1] = incrementIndex(playersCardTails[Player1]);
+
+    }
+
+    private static void friendlyWon() {
+
+        playersCards[Player1][playersCardHeads[Player1]] = playersCardTails[Player1];
+        playersCards[Player2][playersCardHeads[Player2]] = playersCardTails[Player2];
+
+        playersCardTails[Player1] = incrementIndex(playersCardTails[Player1]);
+        playersCardHeads[Player1] = incrementIndex(playersCardHeads[Player1]);
+        playersCardTails[Player2] = incrementIndex(playersCardTails[Player2]);
+        playersCardHeads[Player2] = incrementIndex(playersCardHeads[Player2]);
+    }
+
+    private static void showCountOfCards(int result) {
+
+        int countCardsFirst = playersCardHeads[Player1] - playersCardTails[Player1];
+        int countCardsSecond = playersCardHeads[Player2] - playersCardTails[Player2];
+
+        if (playerCardsIsEmpty(Player1) && result == 0) {
+            System.out.print("У игрока №1 " + 36 + "карт; ");
+        } else if (countCardsFirst >= 0) {
+            System.out.print("У игрока №1 " + countCardsFirst + "карт; ");
+        } else {
+            System.out.print("У игрока №1 " + (playersCardHeads[Player1] + CARDS_TOTAL_COUNT - playersCardTails[Player1]) + "карт; ");
+        }
+
+        if (playerCardsIsEmpty(Player2) && result == 1) {
+            System.out.println("У игрока №1 " + 36  + "карт: ");
+        } else if (countCardsSecond >= 0) {
+            System.out.println(" У игрока №2 " + countCardsSecond + "карт;");
+        } else {
+            System.out.println(" У игрока №2 " + (playersCardHeads[Player2] + CARDS_TOTAL_COUNT - playersCardTails[Player2]) + "карт;");
+        }
+    }
+
+    // Метод, который проверяет на пустоту колоды карт 
+    private static boolean playerCardsIsEmpty(int playerIndex) {
+        int tail = playersCardTails[playerIndex];
+        int head = playersCardHeads[playerIndex];
+
+        return tail == head;
+    }
+    // Методы, который раздает карты игрокам
+    private static void distributionCards() {
+
+        for (int index = 0; index < deckCards.length / 2; index++) {
+            playersCards[Player1][index] = deckCards[index];
+            playersCards[Player2][index] = deckCards[deckCards.length / 2 + index];
+        }
+
+    }
+
+    // Метод, который мешает колоду
+    private static void shufflingCards() {
+
+        for (int index = 0; index < deckCards.length; index++) {
+            deckCards[index] = index;
+        }
+
+        MathArrays.shuffle(deckCards);
+
+    }
+
+    // Метод возвращает размер карты
+    private static int getValueCard(int cardNumber) {
+        return cardNumber % PARS_TOTAL_COUNT;
+    }
+
+    // Возвращает масть карты
     private static Suit getSuit(int cardNumber) {
         return Suit.values()[cardNumber / PARS_TOTAL_COUNT];
     }
 
-    private static Par getPar(int cartNumber) {
-        return Par.values()[cartNumber % PARS_TOTAL_COUNT];
+    // Возвращает размерность карты
+    private static Par getPar(int cardNumber) {
+        return Par.values()[cardNumber % PARS_TOTAL_COUNT];
+    }
+
+    // Информация о карте
+    private static String toString(int cardNumber) {
+        return getPar(cardNumber) + " " + getSuit(cardNumber);
+    }
+
+    // Индекс карты
+    private static int incrementIndex(int i) {
+        return (i + 1) % CARDS_TOTAL_COUNT;
     }
 
 }
